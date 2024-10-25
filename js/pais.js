2c49bf42308fa23460c328b5d930cf1e216254d6
Marcos <Usuario@192.168.1.215>
2024-10-25 20:12:42 +0200

    Actualizar practica

--- js/pais.js
+++ js/pais.js
@@ -1,15 +1,43 @@
-class Pais{
-    constructor(nombre, capital, circuito, poblacion, gobierno, meta){
-
+class Pais {
+    constructor(nombre, capital, circuito, poblacion, gobierno, meta, religion) {
+        this.nombre = nombre;
+        this.capital = capital;
+        this.circuito = circuito;
+        this.poblacion = poblacion;
+        this.gobierno = gobierno;
+        this.meta = meta;
+        this.religion =  religion;
     }
-    obtenerInformacionSecundaria() {
+
+    actualizarAtributos({nombre, capital, circuito, poblacion, gobierno, meta, religion}) {
+        if(nombre) this.nombre = nombre;
+        if(capital) this.capital = capital;
+        if(circuito) this.circuito = circuito;
+        if(poblacion) this.poblacion = poblacion;
+        if(gobierno) this.gobierno = gobierno;
+        if(meta) this.meta = meta;
+        if(religion) this.religion = religion;
+    }
+
+    getNombre() {
+        return `País: ${this.nombre}`;
+    }
+
+    getCapital() {
+        return `Capital: ${this.capital}`;
+    }
+
+    writeCoordenadasMeta() {
+        document.write(`Coordenadas de la meta: ${this.meta}`);
+    }
+
+    getInfoHtml() {
         return `
-            <ul>
-                <li>Nombre del circuito: ${this.circuito}</li>
-                <li>Población: ${this.poblacion}</li>
-                <li>Forma de gobierno: ${this.formaGobierno}</li>
-                <li>Religión mayoritaria: ${this.religionMayoritaria}</li>
-            </ul>
-        `;
+        <ul>
+            <li>Nombre del circuito: ${this.circuito}</li>
+            <li>Población: ${this.poblacion}</li>
+            <li>Forma de gobierno: ${this.gobierno}</li>
+            <li>Religión mayoritaria: ${this.religion}</li>
+        </ul>`;
     }
 }
